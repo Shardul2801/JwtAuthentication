872ea3c2d1f9728ec426e3b4139340ec79dd42f2
Iamneo <techadmin@iamneo.ai>
2023-11-30 08:23:42 +0000

    Backup Commit

--- JwtBackend/Models/User.cs
+++ JwtBackend/Models/User.cs
@@ -8,7 +8,7 @@ namespace JwtBackend.Models
     public class User
     {
         public int UserId {get;set;}
-        public int UserName{get;set;}
+        public string UserName{get;set;}
         public string UserEmail {get;set;}
         public string MobileNumber {get;set;}
         public string Password {get;set;}

a72f525bf00a0cafaa6b1a157f6e67e52c1b6786
Iamneo <techadmin@iamneo.ai>
2023-11-30 06:41:48 +0000

    Backup Commit

--- JwtBackend/Controllers/User.cs
+++ JwtBackend/Controllers/User.cs
@@ -1,8 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AngularAuthYtAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Text;
+using AngularAuthYtAPI.Context;
+using Microsoft.EntityFrameworkCore;
+using AngularAuthYtAPI.Helpers;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using AngularAuthYtAPI.Models;
 
 namespace JwtBackend.Controllers
 {
--- JwtBackend/Models/TokenApi.cs
+++ JwtBackend/Models/TokenApi.cs
@@ -1,3 +1,9 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 namespace JwtBackend.Models;
+
+public class TokenApiDto
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }

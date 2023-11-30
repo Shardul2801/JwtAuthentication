6985c1cb959e476422bf47e00d38b611f277fae7
Iamneo <techadmin@iamneo.ai>
2023-11-30 06:56:48 +0000

    Backup Commit

---
+++ JwtBackend/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using AngularAuthYtAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Text;
+using AngularAuthYtAPI.Context;
+using Microsoft.EntityFrameworkCore;
+using AngularAuthYtAPI.Helpers;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using JwtBackend.Models;
+
+namespace JwtBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public UserController(AppDbContext _context){
+            context = _context;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj){
+             if (userObj == null)
+                return BadRequest();
+
+            var user = await context.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username);
+
+            if (user == null)
+                return NotFound(new { Message = "User not found!" });
+
+            user.Token = CreateJwt(user);
+            var newAccessToken = user.Token;
+            var newRefreshToken = CreateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(5);
+            await _authContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ JwtBackend/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using JwtBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtBackend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().ToTable("users");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        if(!optionsBuilder.IsConfigured){
+            optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123; server=localhost;Database=AuthDb;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+        }
+        }
+    }
+}

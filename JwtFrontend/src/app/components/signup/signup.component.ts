283a991371a708f2292ec398e57d33c36c434320
Iamneo <techadmin@iamneo.ai>
2023-11-30 12:04:40 +0000

    Backup Commit

--- JwtFrontend/src/app/components/signup/signup.component.ts
+++ JwtFrontend/src/app/components/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { AuthService } from 'src/app/services/auth.service';
+import ValidateForm from 'src/app/helpers/validationform';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  public signUpForm!: FormGroup;
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon:string = "fa-eye-slash"
+  constructor(private fb : FormBuilder, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.signUpForm = this.fb.group({
+      UserName:['', Validators.required],
+      UserEmail:['', Validators.required],
+      Password:['', Validators.required]
+    })
   }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash'
+    this.isText ? this.type = 'text' : this.type = 'password'
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+      let signUpObj = {
+        ...this.signUpForm.value,
+        role:'',
+        token:''
+      }
+      this.auth.signUp(signUpObj)
+      .subscribe({
+        next:(res=>{
+          console.log(res.message);
+          this.signUpForm.reset();
+          this.router.navigate(['login']);
+          alert(res.message)
+        }),
+        // error:(err=>{
+        //   alert(err?.error.message)
+        // })
+      })
+    } else {
+      ValidateForm.validateAllFormFields(this.signUpForm); //{7}
+    }
+  }
+
 
 }
